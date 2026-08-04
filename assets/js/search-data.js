889eb2ae97da67b8bbb3d69d65dd137622da15c8
bluestyle97 <bluestyle928@gmail.com>
2026-08-04 14:26:33 +0000

    Deploying to gh-pages from @ bluestyle97/bluestyle97.github.io@650c38a02999443099231b2fd04da1ca2df26c23 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "(*) denotes equal contribution",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
